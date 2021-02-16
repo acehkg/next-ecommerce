6e40fa1726a434b9ab33b90c5531e7d40b397fd3
Andrew Embury <andrewembury@me.com>
2021-02-16 09:03:24 +0800

    add products page

---
+++ pages/products.js
@@ -0,0 +1,17 @@
+const Products = () => {
+  return (
+    <>
+      <h1>Cool Product</h1>
+      <p>This is a cool Product</p>
+      <p>$2.50</p>
+      <h1>Cool Product</h1>
+      <p>This is a cool Product</p>
+      <p>$2.50</p>
+      <h1>Cool Product</h1>
+      <p>This is a cool Product</p>
+      <p>$2.50</p>
+    </>
+  );
+};
+
+export default Products;
